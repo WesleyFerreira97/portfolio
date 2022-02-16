b2477c9fdde348591d5eb1c775b0a64e903c6bb1
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-02-15 23:48:56 -0300

    Changes in typography for responsivity

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import './style/global.css'
-import { useState, useEffect } from 'react'
+import './style/fonts.css'
+import { useState } from 'react'
 import { Carousel } from './components/carousel/index';
 import { Navigation } from './components/navigation/index';
-import { AboutMe } from './pages/aboutMe/index';
+// import { AboutMe } from './pages/aboutMe/index';
 import { Siam } from './pages/siam/index';
 import { AluraCord } from './pages/aluracord/index';
 import { AppFilmes } from './pages/appfilmes/index';
 import { AppProvider } from './providers/AppProvider';
+import { GlobalStyle } from './style/global';
 
 function App() {
   const [index, setIndex] = useState(0);
 
-  return (
+  return ( 
       <AppProvider>
+        <GlobalStyle />
         <Navigation setIndex={setIndex}/>
         <Carousel newIndex={index}>
               <Siam />
