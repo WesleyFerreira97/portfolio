42411723b01613332117af713d0f73153679131c
Wesley Ferreira <wesleyferreira97@outlook.com>
2023-09-11 21:28:57 -0300

    feat: logo with circle animation full

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,25 +8,27 @@ export default function Home() {
     return (
         <div className='flex min-h-screen bg-cover overflow-hidden bg-[#0c0c30] my-fodasea justify-center items-center flex-col'>
             <div className={styles.loading_container}>
-
-                <svg width="762" height="762" viewBox="0 0 762 762" fill="transparent" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        id='W'
-                        d="M174.391 263L270.151 453.38L241.651 510.38L117.771 263H174.391ZM556.292 263H612.912L460.912 567L365.151 376.62L393.651 319.62L460.912 453.38L556.292 263 Z"
-                        fill="#C23373"
-                    />
-                    <clipPath id='clipC'>
-                        <use href='#W' />
-                    </clipPath>
-
-                    <rect
-                        className={styles.cp1}
-                        width="100%"
-                        height="100%"
-                        fill='#fff'
-                        clipPath='url(#clipC)'
-                    />
-                </svg>
+                <span className={styles.loader}>
+                    <svg width="762" height="762" viewBox="0 0 762 762" fill="transparent" xmlns="http://www.w3.org/2000/svg"
+                        style={{ zIndex: 100 }}
+                    >
+                        <path
+                            id='W'
+                            d="M174.391 263L270.151 453.38L241.651 510.38L117.771 263H174.391ZM556.292 263H612.912L460.912 567L365.151 376.62L393.651 319.62L460.912 453.38L556.292 263 Z"
+                            fill="#C23373"
+                        />
+                        <clipPath id='logoW'>
+                            <use href='#W' />
+                        </clipPath>
+                        <rect
+                            className={styles.logoW}
+                            width="100%"
+                            height="100%"
+                            fill='#fff'
+                            clipPath='url(#logoW)'
+                        />
+                    </svg>
+                </span>
             </div>
         </div>
     )
