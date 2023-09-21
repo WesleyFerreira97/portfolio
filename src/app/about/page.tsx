1a815b8aeb2128c951226af4cd797321daa2e520
Wesley Ferreira <wesleyfc00@hotmail.com>
2023-09-21 18:09:18 -0300

    style: create a new home video overlay

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import S from './style.module.css'
+import { relative } from 'path'
+
 const VideoBg = () => {
     return (
-        <video width={"100%"} height={"100%"} autoPlay loop muted>
-            <source src="/videos/bg2.mp4" type='video/mp4' />
-        </video>
+        <div className={S.bgContainer}>
+            <video className={S.video} autoPlay loop muted>
+                <source src="/videos/bg2.mp4" type='video/mp4' />
+            </video>
+            <div className={S.overlay} />
+        </div>
     )
 }
 export default function About() {
     return (
-        <div>
-            About
+        <div className={S.pageContainer}>
             <VideoBg />
+            <div className={S.pageElements}>
+                <h1 className={S.title}>Wesley Ferreira</h1>
+            </div>
         </div>
     )
 }
