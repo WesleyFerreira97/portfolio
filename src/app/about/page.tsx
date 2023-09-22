228bacebef787d4462def3364d5c2293d0919590
Wesley Ferreira <wesleyfc00@hotmail.com>
2023-09-22 16:23:41 -0300

    added component - AboutMe

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import S from './style.module.css'
 import { relative } from 'path'
+import ProfilePic from '../../../public/images/profilePic.jpeg'
+import Image from 'next/image'
 
 const VideoBg = () => {
     return (
@@ -14,24 +16,51 @@ const VideoBg = () => {
 }
 export default function About() {
     return (
-        <div className={S.pageContainer}>
-            <VideoBg />
-            {/* Animação inicial será aplicada ao page elements, com alguma transição para o videoBg */}
-            <div className={S.pageElements}>
-                <div className={S.mainNavbar}>
-                    <span className={S.logo}>
-                        W
-                    </span>
-                    <ul className={S.mainMenu}>
-                        <li>Inicio</li>
-                        <li>Quem sou eu</li>
-                        <li>Projetos</li>
-                        <li>Artigos</li>
-                        <li>Contato</li>
-                    </ul>
+        <>
+            <div className={S.pageContainer}>
+                <VideoBg />
+                {/* Animação inicial será aplicada ao page elements, com alguma transição para o videoBg */}
+                <div className={S.pageElements}>
+                    <div className={S.mainNavbar}>
+                        <span className={S.logo}>
+                            W
+                        </span>
+                        <ul className={S.mainMenu}>
+                            <li>Inicio</li>
+                            <li>Quem sou eu</li>
+                            <li>Projetos</li>
+                            <li>Artigos</li>
+                            <li>Contato</li>
+                        </ul>
+                    </div>
+                    <h1 className={S.title}>Wesley Ferreira</h1>
+                    <span className={S.subtitle}>Desenvolvedor FrontEnd</span>
                 </div>
-                <h1 className={S.title}>Wesley Ferreira</h1>
-                <span className={S.subtitle}>Desenvolvedor FrontEnd</span>
+            </div>
+
+            <AboutMe />
+        </>
+    )
+}
+
+
+const AboutMe = () => {
+    return (
+        <div className={S.aboutMeContainer}>
+            <div className={S.aboutMeThumb}>
+                <Image
+                    src={ProfilePic.src}
+                    fill={true}
+                    alt='Profile Pic'
+                />
+            </div>
+            <div className={S.aboutMeInfo}>
+                <h1 className={S.aboutMeTitle}>
+                    Engenheiro de Software em formação
+                </h1>
+                <p className={S.aboutMeParagraph}>Sou desenvolvedor Mobile e Front-end, voltado a Android Nativo e React Native, com conhecimento aprofundado em React JS/Next, Typescript e proficiência o suficiente para se adaptar as principais bibliotecas e frameworks front-end do mercado como Vue JS e Angular, possuo conhecimentos em design voltado a criação de layouts.</p>
+
+                <p className={S.aboutMeParagraph}>Atualmente estou cursando ensino superior em Engenharia de Software</p>
             </div>
         </div>
     )
