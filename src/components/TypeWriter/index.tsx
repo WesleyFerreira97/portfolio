585a54fb94e01f5c4f88432db030a60d9dbbb4cf
Wesley Ferreira <wesleyferreira97@outlook.com>
2023-09-27 13:13:50 -0300

    feat: typewriter refact

--- src/components/TypeWriter/index.tsx
+++ src/components/TypeWriter/index.tsx
@@ -1,48 +1,57 @@
-import React, { use, useCallback, useEffect, useRef } from 'react'
-
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 type TypeWriterProps = {
     textArray: string[]
 }
 
 export function TypeWriter({ textArray }: TypeWriterProps) {
     const writerRef = useRef<HTMLSpanElement | null>(null);
+    const [isErase, setIsErase] = useState<Boolean>(false)
     let textArrayIndex = 0;
     let charIndex = 0;
 
     const typeAndErase = useCallback(() => {
+        const currentTextTyped = writerRef.current;
 
         const type = () => {
             if (charIndex < textArray[textArrayIndex].length) {
-                const currentTextTyped = writerRef.current;
 
                 if (currentTextTyped) {
                     currentTextTyped.textContent += textArray[textArrayIndex].charAt(charIndex)
-                    console.log(textArray[textArrayIndex].charAt(charIndex), "log aqui");
 
                     charIndex++
-                    setTimeout(type, 400)
+                    setTimeout(typeAndErase, 400)
                 }
             } else {
-                setTimeout(erase, 400)
+
+                setTimeout(erase, 2400)
+                setIsErase(true)
+
             }
         }
 
         const erase = () => {
             console.log("bateuAqui");
 
+            if (charIndex > 0 && currentTextTyped) {
+                currentTextTyped.textContent = textArray[textArrayIndex].substring(0, charIndex - 1)
+                charIndex--
+                setTimeout(erase, 400)
+            }
         }
 
         type()
     }, [charIndex, textArray, textArrayIndex])
 
-
     useEffect(() => {
-        typeAndErase()
-    }, [typeAndErase])
+        if (!isErase) {
+            typeAndErase();
+        }
+    }, [typeAndErase, isErase])
 
     return (
         <>
-            <span ref={writerRef}>{ }</span>
+            <span ref={writerRef}></span>
         </>
     )
 }
+
