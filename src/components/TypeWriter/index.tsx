95e84e3f70ddac62b80b1ec5194829b819a492c4
Wesley Ferreira <wesleyfc00@hotmail.com>
2023-09-28 13:53:14 -0300

    feat: added in component typewrite a rease method, now works correctly

--- src/components/TypeWriter/index.tsx
+++ src/components/TypeWriter/index.tsx
@@ -1,29 +1,45 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react'
+
 type TypeWriterProps = {
     textArray: string[]
 }
 
 export function TypeWriter({ textArray }: TypeWriterProps) {
     const [currentText, setCurrentText] = useState("")
-    let textArrayIndex = 0;
+    let [textArrayIndex, setTextArrayIndex] = useState(0);
     let [charIndex, setCharIndex] = useState(0);
 
     useEffect(() => {
-        let timeout: NodeJS.Timeout;
+        let timeoutTypewrite: NodeJS.Timeout;
+        let timeoutErase: NodeJS.Timeout;
 
         if (charIndex <= textArray[textArrayIndex].length) {
-            timeout = setTimeout(() => {
+            timeoutTypewrite = setTimeout(() => {
                 const currentArrayEl = textArray[textArrayIndex]
                 setCurrentText(prevState => prevState + currentArrayEl.charAt(charIndex))
 
                 setCharIndex(prevState => prevState + 1)
-            }, 300)
+            }, 200)
         } else {
-            console.log("bateu aqui");
+            timeoutErase = setTimeout(() => {
+                eraseText()
+            }, 2500)
+        }
+
+        function eraseText() {
+            setCurrentText("")
+            setCharIndex(0)
+
+            if (textArrayIndex < textArray.length - 1) {
+                setTextArrayIndex(prevState => prevState + 1)
+            } else {
+                setTextArrayIndex(0)
+            }
         }
 
         return () => {
-            clearTimeout(timeout)
+            clearTimeout(timeoutTypewrite)
+            clearTimeout(timeoutErase)
         }
 
     }, [charIndex, textArray, textArrayIndex])
