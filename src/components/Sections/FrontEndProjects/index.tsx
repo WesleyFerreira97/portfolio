5d7f1679637166cbdf290c3924590784330125ae
Wesley Ferreira <wesleyfc00@hotmail.com>
2023-12-04 13:17:37 -0300

    style: card projects overlay e hover effect added

--- src/components/Sections/FrontEndProjects/index.tsx
+++ src/components/Sections/FrontEndProjects/index.tsx
@@ -10,18 +10,29 @@ export function FrontEndProjects() {
             <div className={S.pageHeader}>
 
                 <div className={S.headerInfo}>
-                    <h1 className={S.pageTitle}>Front End</h1>
+                    <div className={S.header}>
+                        <span className={S.iconMobile}>
+                            <Pencil size="100%" />
+                        </span>
+                        <h1 className={S.pageTitle}>Front End</h1>
+                    </div>
                     <p className={S.pageDescription}>Sou desenvolvedor Mobile e Front-end, voltado a Android Nativo e React Native, com conhecimento aprofundado em React JS/Next, Typescript e proficiência o suficiente para se adaptar as principais bibliotecas e frameworks front-end do mercado como Vue JS e Angular, possuo conhecimentos em design voltado a criação de layouts.</p>
 
                     <p className={S.pageDescription}>Atualmente estou cursando ensino superior em Engenharia de Software</p>
                 </div>
-                <div className={S.icon}>
+                <span className={S.icon}>
                     <Pencil size="100%" />
-                </div>
+                </span>
+            </div>
+
+            <div className={S.projectsSection}>
+                <h3 className={S.projectsLabel}>Projetos :</h3>
             </div>
 
             {frontEndProjectsData.map((value, index) => (
-                <CardProject props={value} key={index} />
+                <div key={index} style={{ margin: "3rem 0" }}>
+                    <CardProject props={value} />
+                </div>
             ))}
 
         </>
