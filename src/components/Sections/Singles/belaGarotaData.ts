ee38900944b12fb0ce87a32af45f060ff6df1677
Wesley Ferreira <wesleyfc00@hotmail.com>
2024-01-08 18:27:35 -0300

    .

---
+++ src/components/Sections/Singles/belaGarotaData.ts
@@ -0,0 +1,12 @@
+export const data = {
+    title: "Bela Garota",
+    projectType: "Ecommerce React Native",
+    description: "Bela Garota é uma loja de roupas femininas, onde a cliente pode escolher e comprar as peças de roupa que desejar.",
+    images: {
+        thumbImage: "",
+        mainImage: "",
+        gallery: [
+
+        ]
+    }
+}
