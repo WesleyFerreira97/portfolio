877fce989c9a7e64da0af3d31e6a8b62bc1904dd
Wesley Ferreira <wesleyferreira97@outlook.com>
2024-09-29 05:10:19 -0300

    fix: video home added basepath to directory

--- src/components/HomeVideoBg.tsx
+++ src/components/HomeVideoBg.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 export function HomeVideoBg() {
+    const BASE_PATH = "portfolio"
+    
     return (
         <div className="h-full w-full absolute top-0 left-0 object-cover">
             <video className="absolute top-0 left-0 h-full w-full object-cover" autoPlay loop muted>
-                <source src="/videos/bg2.mp4" type='video/mp4' />
+                <source src={`${BASE_PATH}/videos/bg2.mp4`} type='video/mp4' />
             </video>
             <div className="h-full w-full absolute top-0 left-0 bg-[rgb(5,9,25)]/[.40]" />
         </div>
