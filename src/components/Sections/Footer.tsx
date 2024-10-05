e5889254ed4f587bd54fd571c6605981f11cf3b8
Wesley Ferreira <wesleyferreira97@outlook.com>
2024-10-05 09:14:10 -0300

    style: footer social button

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -2,6 +2,26 @@ import React from 'react'
 import { Container } from '../Layout/Container'
 import { GithubLogo } from '@phosphor-icons/react'
 
+type ItemProps = {
+    icon: React.ReactNode
+    title: string
+    subtitle: string
+}
+
+const ListItem = (props: ItemProps) => {
+    return (
+        <div className='flex flex-col ext-center items-center'>
+            <GithubLogo size={96} alt='github-logo' color='#121a41'/>
+            <span className='text-primary font-primary font-semibold pb-1 pt-2 text-base'>
+                {props.title}
+            </span>
+            <span className='text-white font-primary font-normal text-xs tracking-wide'>
+                {props.subtitle}
+            </span>
+        </div>
+    )
+}
+
 export function Footer() {
   return (
     <div className='flex justify-center h-[55vh] min-h-[400px] bg-secondary '>
@@ -9,14 +29,10 @@ export function Footer() {
             <h3 className='text-[#fff] font-primary font-semibold text-2xl uppercase'>
                 Meus Links
             </h3>
-            <div>
-                <div className=''>
-                    <div className='bg-[#0d122b] p-2 rounded-lg shadow-lg'>
-                        <GithubLogo size={48} alt='github-logo' color='#fff'/>
-                    </div>
-                </div>
-                <span>Github</span>
-                <span>@WesleyFerreira97</span>
+            <div className='flex gap-14'>
+            <ListItem />
+            <ListItem />
+            <ListItem />
             </div>
         </div>
     </div>
