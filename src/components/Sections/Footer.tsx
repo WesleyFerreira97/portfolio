069fa45e1fc5382027c2e0968595b246646d2f69
Wesley Ferreira <wesleyferreira97@outlook.com>
2024-10-05 21:14:38 -0300

    stlye: footer actived item and status text

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -1,36 +1,45 @@
-import React from 'react'
-import { Container } from '../Layout/Container'
-import { GithubLogo, LinkedinLogo, At, ReadCvLogo } from '@phosphor-icons/react'
-import { Icon } from '@phosphor-icons/react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import LogoSVG from '../../../public/images/logo.svg'
-// import CVIcon from '../../../public/images/icons/cv-icon.svg'
-// import GithubIcon from '../../../public/images/icons/github-icon.svg'
-// import LinkedinIcon from '../../../public/images/icons/linkedin-icon.svg'
-import MainIcon from '../../../public/images/icons/mail-icon.svg'
 import { GithubIcon } from '@/assets/GithubIcon'
 import { LinkedinIcon } from '@/assets/LinkedinIcon'
 import { MailIcon } from '@/assets/MailIcon'
 import { CVIcon } from '@/assets/CVIcon'
 
 type ItemProps = {
-    // Icon: Icon
     Icon: any;
-    title: string
-    subtitle: string;
+    isActive: boolean;
+    onClick: () => void;
 }
 
-const ListItem = ({Icon, ...props}: ItemProps) => {
+const ListItem = ({ Icon, ...props }: ItemProps) => {
+  
     return (
-        <div className='flex flex-col items-center'>
-            <Icon fill='#fff' stroke='#fff' color='#fff' width="80px" height="80px" />
-        </div>
-    )
-}
+      <div
+        className='flex flex-col items-center w-[80px] relative group'
+        onClick={props.onClick}
+      >
+        <Icon fill='#fff' stroke='#fff' color='#fff' />
+        <span
+          className={`w-full h-[6px] bg-primary mt-6 absolute -bottom-6 transition-opacity duration-300 ${
+            props.isActive ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'
+          }`}
+        />
+      </div>
+    );
+  };
 
 export function Footer() {
+    const [activeIndex, setActiveIndex] = useState<number | null>(null); 
+    const [currentBarText, setCurrentBarText] = useState<string>()
+
+    const handleItemClick = (index: number, text: string) => {
+      setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
+      setCurrentBarText(text);
+    };
+
   return (
-    <div className='w-full flex flex-col xl:flex-row justify-center h-[40vh] min-h-[400px] bg-[#090D1F]'>
+    <div className='w-full flex flex-col xl:flex-row justify-center h-[40vh] min-h-[400px] bg-[#090D1F] overflow-hidden'>
         <div className='h-full w-full xl:w-[40%] bg-primary flex justify-center items-center flex-col text-center gap-4 p-7'>
             <div className='w-[80px] h-[80px] relative'>
                 <Image
@@ -51,13 +60,31 @@ export function Footer() {
         </div>
         <div className='relative h-full min-h-[300px] w-full xl:w-[60%] bg-secondary flex flex-col justify-center items-center gap-16'>
             <div className='flex items-center justify-center gap-x-24'>
-                <ListItem title='Github' subtitle='@Wesleyferreira97' Icon={GithubIcon} />
-                <ListItem title='LinkedIn' subtitle='@Wesleyferreira97' Icon={LinkedinIcon} />
-                <ListItem title='E-mail' subtitle='Wesleyferreira97@outlook.com' Icon={MailIcon} />
-                <ListItem title='Currículo' subtitle='Download' Icon={CVIcon} />
+            <ListItem
+                Icon={GithubIcon}
+                isActive={activeIndex === 0} // Verifica se é o item ativo
+                onClick={() => handleItemClick(0, "@WesleyFerreira97")} // Define qual item foi clicado
+            />
+            <ListItem
+                Icon={LinkedinIcon}
+                isActive={activeIndex === 1}
+                onClick={() => handleItemClick(1, "@WesleyFerreira97")}
+            />
+            <ListItem
+                Icon={MailIcon}
+                isActive={activeIndex === 2}
+                onClick={() => handleItemClick(2, 'Wesleyferreira97@outlook.com')}
+            />
+            <ListItem
+                Icon={CVIcon}
+                isActive={activeIndex === 3}
+                onClick={() => handleItemClick(3, "Download Currículo")}
+            />
             </div>
             <div className='max-xl:absolute max-xl:bottom-0 w-full flex justify-center items-center bg-[#191B2F] text-white text-center'>
-                <span className='p-2 flex font-primary text-[#ABABAB] text-sm font-semibold'>E-mail -  Wesleyferreira97@outlook.com</span>
+                <span className='p-2 flex font-primary text-[#ABABAB] text-sm font-semibold'>
+                    {currentBarText}
+                </span>
             </div>
         </div>
     </div>
