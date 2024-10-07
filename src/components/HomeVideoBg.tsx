5a35d949d0a5b62bf5ba1416e09fd7b9e8f28765
Wesley Ferreira <wesleyferreira97@outlook.com>
2024-10-06 22:45:41 -0300

    build

--- src/components/HomeVideoBg.tsx
+++ src/components/HomeVideoBg.tsx
@@ -6,7 +6,7 @@ export function HomeVideoBg() {
     return (
         <div className="h-full w-full absolute top-0 left-0 object-cover">
             <video className="absolute top-0 left-0 h-full w-full object-cover" autoPlay loop muted>
-                <source src="portfolio/videos/bg2.mp4" type='video/mp4' />
+                <source src="videos/bg2.mp4" type='video/mp4' />
             </video>
             <div className="h-full w-full absolute top-0 left-0 bg-[rgb(5,9,25)]/[.40]" />
         </div>
